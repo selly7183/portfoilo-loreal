9fd50e32fccad5b0ef724c2b6448bba03f2fb887
selly7183 <selly7183@naver.com>
2022-12-14 20:29:04 +0900

    css commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 "use strict";
+
+const tabLi = document.querySelectorAll(".commitments__tab li");
+const commitmentCont = document.querySelectorAll(".commitments__cont");
+tabLi.forEach((item, index) => {
+	item.addEventListener("click", () => {
+		for (let i = 0; i < tabLi.length; i++) {
+			tabLi[i].classList.remove("on");
+			commitmentCont[i].classList.remove("on");
+		}
+		tabLi[index].classList.add("on");
+		commitmentCont[index].classList.add("on");
+	});
+});
