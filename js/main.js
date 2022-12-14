dfa05d788b6b92a71278d7aeded288d812a7df72
selly7183 <selly7183@naver.com>
2022-12-14 23:37:08 +0900

    js header-fixed, section03-tab,scrollIntoView

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 "use strict";
 
+// header - fixed
+const header = document.querySelector("header");
+const headerRect = header.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+	if (scrollY > headerRect) {
+		header.classList.add("active");
+	} else {
+		header.classList.remove("active");
+	}
+});
+
+// section03 - tab
 const tabLi = document.querySelectorAll(".commitments__tab li");
 const commitmentCont = document.querySelectorAll(".commitments__cont");
 tabLi.forEach((item, index) => {
@@ -11,4 +23,11 @@ tabLi.forEach((item, index) => {
 		tabLi[index].classList.add("on");
 		commitmentCont[index].classList.add("on");
 	});
+});
+
+// section03 - scrollIntoView
+const commitments = document.querySelector(".link_sec03");
+const section03 = document.querySelector(".section03");
+commitments.addEventListener("click", (e) => {
+	section03.scrollIntoView({ behavior: "smooth", block: "start" });
 });
