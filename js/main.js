5312f2d2163cff8205be78f1c8a27c8586989e2e
selly7183 <selly7183@naver.com>
2022-12-16 16:44:17 +0900

    js locomotive scroll smooth

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
 "use strict";
+//scroll smooth
+gsap.registerPlugin(ScrollTrigger);
+const scroll = new LocomotiveScroll({
+	el: document.querySelector("[data-scroll-container]"),
+	smooth: true,
+	lerp: 0.05,
+	scrollFromAnywhere: true,
+});
+
+ScrollTrigger.create({
+	trigger: ".image__cont_box",
+	scroll: "[data-scroll-container]",
+	start: "top+=30% 50%",
+	end: "bottom-=40% 50%",
+	animation: gsap.to(".image__cont_box img", { with: "120%" }),
+	scrub: 2,
+	// markers: true
+});
 
 // preload
 const preloader = document.querySelector(".preloader");
@@ -38,25 +56,30 @@ commitments.addEventListener("click", (e) => {
 	section03.scrollIntoView({ behavior: "smooth", block: "start" });
 });
 
-//section03 - line
-const section03Rect = section03.getBoundingClientRect().height;
-console.log(section03Rect);
-const tab = document.querySelector(".commitments__tab");
-document.addEventListener("scroll", (e) => {
-	if (window.scrollY > section03Rect * 2.5) {
-		tab.classList.add("on");
-	} else {
-		tab.classList.remove("on");
-	}
-});
+//section04 - mouse cursor
+(function () {
+	var parallaxContainer = document.querySelector(".section04");
 
-// section04 - mouse cursor
-// const section04 = document.querySelector(".section04");
+	var parallaxItems = document.querySelector(".img__box");
 
-// document.addEventListener("mousemove", (e) => {
-// 	const x = e.clientX;
-// 	const y = e.clientY;
-// 	const cursor = document.querySelector("#cursor");
-// 	section04.style.cursor = "none";
-// 	cursor.style.transform = `translate(${x}px, ${y}px )`;
-// });
+	var fixer = 0.003;
+
+	document.addEventListener("mousemove", function (event) {
+		var pageX =
+			event.pageX - parallaxContainer.getBoundingClientRect().width * 0.5;
+
+		var pageY =
+			event.pageY - parallaxContainer.getBoundingClientRect().height * 0.5;
+
+		for (let i = 0; i < parallaxItems.length; i++) {
+			var item = parallaxItems[i];
+			var speedX = item.getAttribute("data-speed-x");
+			var speedY = item.getAttribute("data-speed-y");
+
+			TweenLite.set(item, {
+				x: (item.offsetLeft + pageX * speedX) * fixer,
+				y: (item.offsetTop + pageY * speedY) * fixer,
+			});
+		}
+	});
+})();
