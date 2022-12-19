ad4a149e40215c8c3c6b006607d2c4af293f3677
selly7183 <selly7183@naver.com>
2022-12-20 01:22:31 +0900

    about scroll error 수정

--- js/about.js
+++ js/about.js
@@ -1,5 +1,9 @@
 "use strict";
 
+const body = document.querySelector("body");
+window.addEventListener("load", (e) => {
+	body.classList.add("about");
+});
 // const txt = document.querySelector(".ceo__txt");
 // window.addEventListener("load", (e) => {
 // 	setTimeout(() => {
