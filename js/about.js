f630a60eb8b87ce0e9c6cee6452f7687b1bd992f
selly7183 <selly7183@naver.com>
2022-12-20 14:07:37 +0900

    about layout & transition

--- js/about.js
+++ js/about.js
@@ -4,9 +4,9 @@ const body = document.querySelector("body");
 window.addEventListener("load", (e) => {
 	body.classList.add("about");
 });
-// const txt = document.querySelector(".ceo__txt");
-// window.addEventListener("load", (e) => {
-// 	setTimeout(() => {
-// 		txt.classList.add("is-inview");
-// 	}, 1700);
-// });
+const txt = document.querySelector(".ceo__txt");
+window.addEventListener("load", (e) => {
+	setTimeout(() => {
+		txt.classList.add("is-inview");
+	}, 1700);
+});
