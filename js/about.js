45c786264cb6aaab9e5deac97634a6623c934197
selly7183 <selly7183@naver.com>
2022-12-24 09:17:53 +0900

    brand swiper

--- js/about.js
+++ js/about.js
@@ -20,3 +20,25 @@ window.addEventListener("load", (e) => {
 		brandDesc?.classList.add("is-inview");
 	}, 2100);
 });
+
+const swiper = new Swiper(".swiper", {
+	// Optional parameters
+	// direction: "vertical",
+	loop: true,
+
+	// If we need pagination
+	pagination: {
+		el: ".swiper-pagination",
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+
+	// And if we need scrollbar
+	scrollbar: {
+		el: ".swiper-scrollbar",
+	},
+});
