bf481635d74d407914bc29f3bdb64c4ac8c5e5a0
selly7183 <selly7183@naver.com>
2022-12-27 18:01:28 +0900

    script 추가

--- js/main.js
+++ js/main.js
@@ -87,3 +87,23 @@ function mouseMoveFunc(e) {
 		overwrite: true,
 	});
 }
+
+// iphone screen Height
+function setScreenSize() {
+	// let vh = window.innerHeight * 0.01;
+
+	// document.documentElement.style.setProperty('--vh', `${vh}px`);
+	let vh = window.innerHeight * 0.01;
+	// Then we set the value in the --vh custom property to the root of the document
+	document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+	// We listen to the resize event
+	window.addEventListener("resize", () => {
+		// We execute the same script as before
+		let vh = window.innerHeight * 0.01;
+		document.documentElement.style.setProperty("--vh", `${vh}px`);
+	});
+}
+
+setScreenSize();
+window.addEventListener("resize", () => setScreenSize());
